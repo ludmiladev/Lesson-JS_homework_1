f15bd3151351351262d27a9ea8fd967185edf004
ludmiladev <ljuda12@gmail.com>
2023-02-01 16:48:03 +0300

    Перше завдання

---
+++ assets.js
@@ -0,0 +1,52 @@
+// - Створити змінні. Присвоїти кожному з них значення: 'hello','owu','com', 'ua', 1, 10, -999, 123, 3.14, 2.7, 16, true, false.
+//     Виведіть кожну зміну за допомогою: console.log
+
+let a; // oб'явили змінну
+a = 'hello'; // надали змінній а значення //
+console.log(a);  // виводить значення а //
+
+let b;
+b = 'owu';
+console.log(b);
+
+let c;
+c = 'owu';
+console.log(c);
+
+let d;
+d = 'owu';
+console.log(d);
+// можна перевизначити змінну d//
+d = 'com';
+console.log(d);
+
+d = 1;
+console.log(d);
+
+d = 10;
+console.log(d);
+
+d = -999;
+console.log(d);
+
+d = 123;
+console.log(d);
+
+d = 3.14;
+console.log(d);
+
+d = 2.7;
+console.log(d);
+
+d = 16;
+console.log(d);
+
+d = true ;
+console.log(d)
+
+d = false ;
+console.log(d)
+
+
+
+
