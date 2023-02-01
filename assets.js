73c45483b4f90e8f2bc4ff9dd99859323cb978ac
ludmiladev <ljuda12@gmail.com>
2023-02-01 19:15:49 +0300

    завдання

--- assets.js
+++ assets.js
@@ -1,4 +1,4 @@
-// - Створити змінні. Присвоїти кожному з них значення: 'hello','owu','com', 'ua', 1, 10, -999, 123, 3.14, 2.7, 16, true, false.
+// - Завдання №1 Створити змінні. Присвоїти кожному з них значення: 'hello','owu','com', 'ua', 1, 10, -999, 123, 3.14, 2.7, 16, true, false.
 //     Виведіть кожну зміну за допомогою: console.log
 
 let a; // oб'явили змінну
@@ -16,7 +16,7 @@ console.log(c);
 let d;
 d = 'owu';
 console.log(d);
-// можна перевизначити змінну d//
+// можна перевизначити змінну d
 d = 'com';
 console.log(d);
 
@@ -41,12 +41,44 @@ console.log(d);
 d = 16;
 console.log(d);
 
-d = true ;
-console.log(d)
+d = true;
+console.log(d);
 
-d = false ;
-console.log(d)
+d = false;
+console.log(d);
 
+// Завдання №2
+// Створити 3 змінних Ім'я, По батькові, Прізвище, наповнити їх своїм ПІБ.  ' +
+// З'єднати їх в одну змінну особу (Не об'єкт, просто для допомоги конкатенації)
+
+let surname = 'Odud';
+let name = 'Oleg';
+let otchestvo = 'Opanasenko';
+
+console.log(surname + ' ' + name + ' ' + otchestvo);
+
+// Або по іншому
+
+let fio = surname + name + otchestvo;
+console.log(fio);
+
+//Завдання №3
+// За допомогою оператора typeof налаштування типи наступних змінних та вивести їх у консоль.
+// нехай a1 = 100; нехай b1 = '100'; нехай c1 = істина;
+
+let a1 = 100;
+let b1 = '100';
+let c1 = "істина";
+console.log(typeof a1);
+console.log(typeof b1);
+console.log(typeof c1);
+
+// Додаткове завдання
+// За допомогою 3х різних prompt() отримати 3 слова, які є вашими Ім'ями, По-Батьківськими та роками.
+// та вивести в консоль
+
+    let name1 = prompt("Як вас звати", " ");
+    console.log("Привіт"+name1);
 
 
 
